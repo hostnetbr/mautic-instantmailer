1798e316dfecea20a2f3434b1b42229e71d5433b
Henrique Rodrigues <henrique@hostnet.com.br>
2018-04-27 14:16:16 -0300

    New icon

--- HostnetMailerBundle/Config/config.php
+++ HostnetMailerBundle/Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Mailer Configuration',
     'description' => 'Filter sending of messages on Mautic.',
-    'version'     => '2.0.0',
+    'version'     => '2.1.0',
     'author'      => 'Henrique Rodrigues',
     'routes'      => [
     ],
